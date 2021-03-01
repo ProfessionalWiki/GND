108168741e1a1768bff59efeb813591e0c0ebec9
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-03-01 18:55:40 +0100

    Add item id support

--- src/Adapters/DataAccess/GndConverter/ProductionValueBuilder.php
+++ src/Adapters/DataAccess/GndConverter/ProductionValueBuilder.php
@@ -6,11 +6,22 @@ namespace DNB\GND\Adapters\DataAccess\GndConverter;
 
 use DataValues\DataValue;
 use DataValues\StringValue;
+use Wikibase\DataModel\Entity\EntityIdValue;
+use Wikibase\DataModel\Entity\ItemId;
 
 class ProductionValueBuilder implements ValueBuilder {
 
+	// TODO: error cases
 	public function stringToDataValue( string $value, string $typeId ): DataValue {
+		if ( $typeId === 'wikibase-entityid' ) {
+			return $this->stringToEntityIdValue( $value );
+		}
+
 		return new StringValue( $value );
+	}
+
+	private function stringToEntityIdValue( string $value ): EntityIdValue {
+		return new EntityIdValue( new ItemId( $value ) );
 	}
 
 }
--- tests/Unit/Adapters/DataAccess/GndConverter/ProductionValueBuilderTest.php
+++ tests/Unit/Adapters/DataAccess/GndConverter/ProductionValueBuilderTest.php
@@ -7,6 +7,8 @@ namespace DNB\GND\Tests\Unit\Adapters\DataAccess\GndConverter;
 use DataValues\StringValue;
 use DNB\GND\Adapters\DataAccess\GndConverter\ProductionValueBuilder;
 use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Entity\EntityIdValue;
+use Wikibase\DataModel\Entity\ItemId;
 
 /**
  * @covers \DNB\GND\Adapters\DataAccess\GndConverter\ProductionValueBuilder
@@ -22,6 +24,13 @@ class ProductionValueBuilderTest extends TestCase {
 
 	private function newBuilder(): ProductionValueBuilder {
 		return new ProductionValueBuilder();
+	}
+
+	public function testItemIdValue(): void {
+		$this->assertEquals(
+			new EntityIdValue( new ItemId( 'Q42' ) ),
+			$this->newBuilder()->stringToDataValue( 'Q42', 'wikibase-entityid' )
+		);
 	}
 
 }
